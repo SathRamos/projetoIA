47640fb1d9590307ebd0c8dffec46353020294c1
Lesath Ramos <ramoslesath@gmail.com>
2021-01-29 00:11:45 -0300

    Utilizacao de apenas uma tabela com todos os dados. Inclui a classe como uma caracteristica. Tratamentos finais no dataset. Criaçao e aplicacao inicial do algoritmo k-nn

--- assemble_feature_vectors.py
+++ assemble_feature_vectors.py
@@ -12,24 +12,9 @@ current = os.getcwd()
 os.chdir(current)
 result = glob.glob('*.{}'.format('csv'))
 
-lmp1_2016 = np.empty((0,18), str)
-lmp1_2017 = np.empty((0,18), str)
-lmp1_2018 = np.empty((0,18), str)
-lmp1_2019 = np.empty((0,18), str)
-lmp2_2016 = np.empty((0,18), str)
-lmp2_2017 = np.empty((0,18), str)
-lmp2_2018 = np.empty((0,18), str)
-lmp2_2019 = np.empty((0,18), str)
-gtePro_2016 = np.empty((0,18), str)
-gtePro_2017 = np.empty((0,18), str)
-gtePro_2018 = np.empty((0,18), str)
-gtePro_2019 = np.empty((0,18), str)
-gteAm_2016 = np.empty((0,18), str)
-gteAm_2017 = np.empty((0,18), str)
-gteAm_2018 = np.empty((0,18), str)
-gteAm_2019 = np.empty((0,18), str)
+dataSet = np.empty((0,18), str)
 
-firstLine = np.array(['id', 'pos_quali_0', 'pos_quali_1', 'pos_quali_2', 'pos_quali_3',
+firstLine = np.array(['id', 'class' 'pos_quali_0', 'pos_quali_1', 'pos_quali_2', 'pos_quali_3',
                                 'laps_quali_0', 'laps_quali_1', 'laps_quali_2', 'laps_quali_3', 
                                 'time_quali_0', 'time_quali_1', 'time_quali_2', 'time_quali_3', 
                                 'kph_quali_0', 'kph_quali_1', 'kph_quali_2', 'kph_quali_3', 'pos_race'], dtype=object)
@@ -56,353 +41,56 @@ for table in result:
         itemNum = row.loc["No"]
         itemClass = row.loc["Class"]
         itemClass = itemClass.replace(" ", "_")
+        itemClass = itemClass.lower()
         itemLaps = row.loc["Laps"]
-        itemTime = row.loc["Time"]
-        itemKph = row.loc["Kph"]
+        itemTime = str(row.loc["Time"])
+        itemTime = itemTime[:-4]
+        itemTime = itemTime.replace(":", ".")
+        itemKph = "{:.3f}".format(row.loc["Kph"])
 
-        #print(itemClass)
+        #define categorias por número
+        if itemClass == "lmp1-h": itemClass = 0
+        elif itemClass == "lmp1": itemClass = 1
+        elif itemClass == "lmp2": itemClass = 2
+        elif itemClass == "gte_pro": itemClass = 3
+        elif itemClass == "gte_am": itemClass = 4
 
-        if year == "2016":
-            if itemClass == "LMP1":
-                print("check LMP1")
-                if itemNum not in lmp1_2016[:,0]:
-                        lmp1_2016 = np.append(lmp1_2016, np.array([[itemNum, "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""]], dtype=object), axis=0)
-
-                y_coord = np.where(lmp1_2016[:,0] == itemNum)
-                x_coord = np.where(firstLine == "pos_"+event)
-                lmp1_2016[y_coord, x_coord] = itemPos
-
-                x_coord = np.where(firstLine == "laps_"+event)
-                lmp1_2016[y_coord, x_coord] = itemLaps
-
-                x_coord = np.where(firstLine == "time_"+event)
-                lmp1_2016[y_coord, x_coord] = itemTime
-
-                x_coord = np.where(firstLine == "kph_"+event)
-                lmp1_2016[y_coord, x_coord] = itemKph
-                
-            elif itemClass == "LMP2":
-                print("check LMP2")
-                if itemNum not in lmp2_2016[:,0]:
-                        lmp2_2016 = np.append(lmp2_2016, np.array([[itemNum, "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""]], dtype=object), axis=0)
-
-                y_coord = np.where(lmp2_2016[:,0] == itemNum)
-                x_coord = np.where(firstLine == "pos_"+event)
-                lmp2_2016[y_coord, x_coord] = itemPos
-
-                x_coord = np.where(firstLine == "laps_"+event)
-                lmp2_2016[y_coord, x_coord] = itemLaps
-
-                x_coord = np.where(firstLine == "time_"+event)
-                lmp2_2016[y_coord, x_coord] = itemTime
-
-                x_coord = np.where(firstLine == "kph_"+event)
-                lmp2_2016[y_coord, x_coord] = itemKph
-
-            elif itemClass == "GTE_Pro":
-                print("check GTE_Pro")
-                if itemNum not in gtePro_2016[:,0]:
-                        gtePro_2016 = np.append(gtePro_2016, np.array([[itemNum, "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""]], dtype=object), axis=0)
-
-                y_coord = np.where(gtePro_2016[:,0] == itemNum)
-                x_coord = np.where(firstLine == "pos_"+event)
-                gtePro_2016[y_coord, x_coord] = itemPos
-
-                x_coord = np.where(firstLine == "laps_"+event)
-                gtePro_2016[y_coord, x_coord] = itemLaps
-
-                x_coord = np.where(firstLine == "time_"+event)
-                gtePro_2016[y_coord, x_coord] = itemTime
-
-                x_coord = np.where(firstLine == "kph_"+event)
-                gtePro_2016[y_coord, x_coord] = itemKph
-
-            elif itemClass == "GTE_Am":
-                print("check GTE_Am")
-                if itemNum not in gteAm_2016[:,0]:
-                        gteAm_2016 = np.append(gteAm_2016, np.array([[itemNum, "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""]], dtype=object), axis=0)
-
-                y_coord = np.where(gteAm_2016[:,0] == itemNum)
-                x_coord = np.where(firstLine == "pos_"+event)
-                gteAm_2016[y_coord, x_coord] = itemPos
-
-                x_coord = np.where(firstLine == "laps_"+event)
-                gteAm_2016[y_coord, x_coord] = itemLaps
-
-                x_coord = np.where(firstLine == "time_"+event)
-                gteAm_2016[y_coord, x_coord] = itemTime
-
-                x_coord = np.where(firstLine == "kph_"+event)
-                gteAm_2016[y_coord, x_coord] = itemKph
+        if itemNum not in dataSet[:,0]:
+                dataSet = np.append(dataSet, np.array([[itemNum, itemClass, "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""]], dtype=object), axis=0)
         
-        elif year == "2017":
-            if itemClass == "LMP1":
-                if itemNum not in lmp1_2017[:,0]:
-                        lmp1_2017 = np.append(lmp1_2017, np.array([[itemNum, "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""]], dtype=object), axis=0)
+        y_coord = np.where(dataSet[:,0] == itemNum)
+        x_coord = np.where(firstLine == "pos_"+event)
+        dataSet[y_coord, x_coord] = itemPos
+
+        x_coord = np.where(firstLine == "laps_"+event)
+        dataSet[y_coord, x_coord] = itemLaps
+
+        x_coord = np.where(firstLine == "time_"+event)
+        dataSet[y_coord, x_coord] = itemTime
+
+        x_coord = np.where(firstLine == "kph_"+event)
+        dataSet[y_coord, x_coord] = itemKph
         
-                y_coord = np.where(lmp1_2017[:,0] == itemNum)
-                x_coord = np.where(firstLine == "pos_"+event)
-                lmp1_2017[y_coord, x_coord] = itemPos
-
-                x_coord = np.where(firstLine == "laps_"+event)
-                lmp1_2017[y_coord, x_coord] = itemLaps
-
-                x_coord = np.where(firstLine == "time_"+event)
-                lmp1_2017[y_coord, x_coord] = itemTime
-
-                x_coord = np.where(firstLine == "kph_"+event)
-                lmp1_2017[y_coord, x_coord] = itemKph
                 
-            elif itemClass == "LMP2":
-                if itemNum not in lmp2_2017[:,0]:
-                        lmp2_2017 = np.append(lmp2_2017, np.array([[itemNum, "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""]], dtype=object), axis=0)
-
-                y_coord = np.where(lmp2_2017[:,0] == itemNum)
-                x_coord = np.where(firstLine == "pos_"+event)
-                lmp2_2017[y_coord, x_coord] = itemPos
-
-                x_coord = np.where(firstLine == "laps_"+event)
-                lmp2_2017[y_coord, x_coord] = itemLaps
-
-                x_coord = np.where(firstLine == "time_"+event)
-                lmp2_2017[y_coord, x_coord] = itemTime
-
-                x_coord = np.where(firstLine == "kph_"+event)
-                lmp2_2017[y_coord, x_coord] = itemKph  
-
-            elif itemClass == "GTE_Pro":
-                if itemNum not in gtePro_2017[:,0]:
-                        gtePro_2017 = np.append(gtePro_2017, np.array([[itemNum, "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""]], dtype=object), axis=0)
-
-                y_coord = np.where(gtePro_2017[:,0] == itemNum)
-                x_coord = np.where(firstLine == "pos_"+event)
-                gtePro_2017[y_coord, x_coord] = itemPos
-
-                x_coord = np.where(firstLine == "laps_"+event)
-                gtePro_2017[y_coord, x_coord] = itemLaps
-
-                x_coord = np.where(firstLine == "time_"+event)
-                gtePro_2017[y_coord, x_coord] = itemTime
-
-                x_coord = np.where(firstLine == "kph_"+event)
-                gtePro_2017[y_coord, x_coord] = itemKph  
-
-            elif itemClass == "GTE_Am":
-                if itemNum not in gteAm_2017[:,0]:
-                        gteAm_2017 = np.append(gteAm_2017, np.array([[itemNum, "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""]], dtype=object), axis=0)
-
-                y_coord = np.where(gteAm_2017[:,0] == itemNum)
-                x_coord = np.where(firstLine == "pos_"+event)
-                gteAm_2017[y_coord, x_coord] = itemPos
-
-                x_coord = np.where(firstLine == "laps_"+event)
-                gteAm_2017[y_coord, x_coord] = itemLaps
-
-                x_coord = np.where(firstLine == "time_"+event)
-                gteAm_2017[y_coord, x_coord] = itemTime
-
-                x_coord = np.where(firstLine == "kph_"+event)
-                gteAm_2017[y_coord, x_coord] = itemKph  
-
-        elif year == "2018":
-            if itemClass == "LMP1":
-                if itemNum not in lmp1_2018[:,0]:
-                        print("***Added new row***")
-                        print(itemNum)
-                        lmp1_2018 = np.append(lmp1_2018, np.array([[itemNum, "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""]], dtype=object), axis=0)
-                        dataframe = pd.DataFrame(lmp1_2018)
-                        print(dataframe)
-
-                #print("***Insert pos in existing row***")
-                #print(itemNum)
-                y_coord = np.where(lmp1_2018[:,0] == itemNum)
-                x_coord = np.where(firstLine == "pos_"+event)
-                #print(x_coord)
-                #print(y_coord)
-                lmp1_2018[y_coord, x_coord] = itemPos
-                dataframe = pd.DataFrame(lmp1_2018)
-                #print(dataframe)
-
-                #print("***Insert laps in existing row***")
-                #print(itemNum)
-                x_coord = np.where(firstLine == "laps_"+event)
-                #print(x_coord)
-                #print(y_coord)
-                lmp1_2018[y_coord, x_coord] = itemLaps
-                dataframe = pd.DataFrame(lmp1_2018)
-                #print(dataframe)
-
-                #print("***Insert time in existing row***")
-                #print(itemNum)
-                x_coord = np.where(firstLine == "time_"+event)
-                #print(x_coord)
-                #print(y_coord)
-                lmp1_2018[y_coord, x_coord] = itemTime
-                dataframe = pd.DataFrame(lmp1_2018)
-                #print(dataframe)
-
-                #print("***Insert kph in existing row***")
-                #print(itemNum)
-                x_coord = np.where(firstLine == "kph_"+event)
-                #print(x_coord)
-                #print(y_coord)
-                lmp1_2018[y_coord, x_coord] = itemKph
-                dataframe = pd.DataFrame(lmp1_2018)
-                #print(dataframe)
-                
-            elif itemClass == "LMP2":
-                if itemNum not in lmp2_2018[:,0]:
-                        lmp2_2018 = np.append(lmp2_2018, np.array([[itemNum, "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""]], dtype=object), axis=0)
-
-                y_coord = np.where(lmp2_2018[:,0] == itemNum)
-                x_coord = np.where(firstLine == "pos_"+event)
-                lmp2_2018[y_coord, x_coord] = itemPos
-
-                x_coord = np.where(firstLine == "laps_"+event)
-                lmp2_2018[y_coord, x_coord] = itemLaps
-
-                x_coord = np.where(firstLine == "time_"+event)
-                lmp2_2018[y_coord, x_coord] = itemTime
-
-                x_coord = np.where(firstLine == "kph_"+event)
-                lmp2_2018[y_coord, x_coord] = itemKph  
-
-            elif itemClass == "GTE_Pro":
-                if itemNum not in gtePro_2018[:,0]:
-                        gtePro_2018 = np.append(gtePro_2018, np.array([[itemNum, "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""]], dtype=object), axis=0)
-
-                y_coord = np.where(gtePro_2018[:,0] == itemNum)
-                x_coord = np.where(firstLine == "pos_"+event)
-                gtePro_2018[y_coord, x_coord] = itemPos
-
-                x_coord = np.where(firstLine == "laps_"+event)
-                gtePro_2018[y_coord, x_coord] = itemLaps
-
-                x_coord = np.where(firstLine == "time_"+event)
-                gtePro_2018[y_coord, x_coord] = itemTime
-
-                x_coord = np.where(firstLine == "kph_"+event)
-                gtePro_2018[y_coord, x_coord] = itemKph    
-
-            elif itemClass == "GTE_Am":
-                if itemNum not in gteAm_2018[:,0]:
-                        gteAm_2018 = np.append(gteAm_2018, np.array([[itemNum, "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""]], dtype=object), axis=0)
-
-                y_coord = np.where(gteAm_2018[:,0] == itemNum)
-                x_coord = np.where(firstLine == "pos_"+event)
-                gteAm_2018[y_coord, x_coord] = itemPos
-
-                x_coord = np.where(firstLine == "laps_"+event)
-                gteAm_2018[y_coord, x_coord] = itemLaps
-
-                x_coord = np.where(firstLine == "time_"+event)
-                gteAm_2018[y_coord, x_coord] = itemTime
-
-                x_coord = np.where(firstLine == "kph_"+event)
-                gteAm_2018[y_coord, x_coord] = itemKph      
-        
-        elif year == "2019":
-            if itemClass == "LMP1":
-                if itemNum not in lmp1_2019[:,0]:
-                        lmp1_2019 = np.append(lmp1_2019, np.array([[itemNum, "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""]], dtype=object), axis=0)
-
-                y_coord = np.where(lmp1_2019[:,0] == itemNum)
-                x_coord = np.where(firstLine == "pos_"+event)
-                lmp1_2019[y_coord, x_coord] = itemPos
-
-                x_coord = np.where(firstLine == "laps_"+event)
-                lmp1_2019[y_coord, x_coord] = itemLaps
-
-                x_coord = np.where(firstLine == "time_"+event)
-                lmp1_2019[y_coord, x_coord] = itemTime
-
-                x_coord = np.where(firstLine == "kph_"+event)
-                lmp1_2019[y_coord, x_coord] = itemKph      
-                
-            elif itemClass == "LMP2":
-                if itemNum not in lmp2_2019[:,0]:
-                        lmp2_2019 = np.append(lmp2_2019, np.array([[itemNum, "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""]], dtype=object), axis=0)
-                
-                y_coord = np.where(lmp2_2019[:,0] == itemNum)
-                x_coord = np.where(firstLine == "pos_"+event)
-                lmp2_2019[y_coord, x_coord] = itemPos
-
-                x_coord = np.where(firstLine == "laps_"+event)
-                lmp2_2019[y_coord, x_coord] = itemLaps
-
-                x_coord = np.where(firstLine == "time_"+event)
-                lmp2_2019[y_coord, x_coord] = itemTime
-
-                x_coord = np.where(firstLine == "kph_"+event)
-                lmp2_2019[y_coord, x_coord] = itemKph  
-
-            elif itemClass == "GTE_Pro":
-                if itemNum not in gtePro_2019[:,0]:
-                        gtePro_2019 = np.append(gtePro_2019, np.array([[itemNum, "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""]], dtype=object), axis=0)
-                
-                y_coord = np.where(gtePro_2019[:,0] == itemNum)
-                x_coord = np.where(firstLine == "pos_"+event)
-                gtePro_2019[y_coord, x_coord] = itemPos
-
-                x_coord = np.where(firstLine == "laps_"+event)
-                gtePro_2019[y_coord, x_coord] = itemLaps
-
-                x_coord = np.where(firstLine == "time_"+event)
-                gtePro_2019[y_coord, x_coord] = itemTime
-
-                x_coord = np.where(firstLine == "kph_"+event)
-                gtePro_2019[y_coord, x_coord] = itemKph  
-
-            elif itemClass == "GTE_Am":
-                if itemNum not in gteAm_2019[:,0]:
-                        gteAm_2019 = np.append(gteAm_2019, np.array([[itemNum, "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""]], dtype=object), axis=0)
-
-                y_coord = np.where(gteAm_2019[:,0] == itemNum)
-                x_coord = np.where(firstLine == "pos_"+event)
-                gteAm_2019[y_coord, x_coord] = itemPos
-
-                x_coord = np.where(firstLine == "laps_"+event)
-                gteAm_2019[y_coord, x_coord] = itemLaps
-
-                x_coord = np.where(firstLine == "time_"+event)
-                gteAm_2019[y_coord, x_coord] = itemTime
-
-                x_coord = np.where(firstLine == "kph_"+event)
-                gteAm_2019[y_coord, x_coord] = itemKph  
-
-df = pd.DataFrame(lmp2_2017)
+df = pd.DataFrame(dataSet)
 print(df)
 
 
-df = pd.DataFrame(lmp1_2016)
-df.to_csv('new_tables/lmp1_2016.csv', index=True)
-df = pd.DataFrame(lmp1_2017)
-df.to_csv('new_tables/lmp1_2017.csv', index=True)
-df = pd.DataFrame(lmp1_2018)
-df.to_csv('new_tables/lmp1_2018.csv', index=True)
-df = pd.DataFrame(lmp1_2019)
-df.to_csv('new_tables/lmp1_2019.csv', index=True)
-df = pd.DataFrame(lmp2_2016)
-df.to_csv('new_tables/lmp2_2016.csv', index=True)
-df = pd.DataFrame(lmp2_2017)
-df.to_csv('new_tables/lmp2_2017.csv', index=True)
-df = pd.DataFrame(lmp2_2018)
-df.to_csv('new_tables/lmp2_2018.csv', index=True)
-df = pd.DataFrame(lmp2_2019)
-df.to_csv('new_tables/lmp2_2019.csv', index=True)
-df = pd.DataFrame(gtePro_2016)
-df.to_csv('new_tables/gtePro_2016.csv', index=True)
-df = pd.DataFrame(gtePro_2017)
-df.to_csv('new_tables/gtePro_2017.csv', index=True)
-df = pd.DataFrame(gtePro_2018)
-df.to_csv('new_tables/gtePro_2018.csv', index=True)
-df = pd.DataFrame(gtePro_2019)
-df.to_csv('new_tables/gtePro_2019.csv', index=True)
-df = pd.DataFrame(gteAm_2016)
-df.to_csv('new_tables/gteAm_2016.csv', index=True)
-df = pd.DataFrame(gteAm_2017)
-df.to_csv('new_tables/gteAm_2017.csv', index=True)
-df = pd.DataFrame(gteAm_2018)
-df.to_csv('new_tables/gteAm_2018.csv', index=True)
-df = pd.DataFrame(gteAm_2019)
-df.to_csv('new_tables/gteAm_2019.csv', index=True)
+df = pd.DataFrame(dataSet)
+df.to_csv('data_set.csv', index=True)
+
+#transforma o DNF em 100 (significa que terminou numa posição acima dos outros)
+numRows = df.shape[0]
+
+if 'DNF' in df.values:
+    df = df.replace(['DNF', "NC", "DSQ"],'100')
+
+#remove linhas com dados faltando (NaN)
+df = df.dropna()
+print("***apos remocao de NaN")
+
+#del df['Unnamed: 0']
+
+print(df)
+
+df.to_csv('data_set.csv', index=True)
--- knn.py
+++ knn.py
@@ -3,30 +3,39 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import os
 
-headerNames = np.array(['id', 'pos_quali_0', 'pos_quali_1', 'pos_quali_2', 'pos_quali_3',
-                                'laps_quali_0', 'laps_quali_1', 'laps_quali_2', 'laps_quali_3', 
-                                'time_quali_0', 'time_quali_1', 'time_quali_2', 'time_quali_3', 
-                                'kph_quali_0', 'kph_quali_1', 'kph_quali_2', 'kph_quali_3', 'pos_race'])
+#algoritmo 1-nn
+# Importa o  dataset
+dataset = pd.read_csv('data_set.csv')
+dataset = dataset.dropna()
+X = dataset.iloc[:, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]].values
+y = dataset.iloc[:, -1].values
 
-for table in os.scandir('new_tables/'):
-    #
-    #y_coord = np.where([:,0] == "DNF")
-    df = pd.read_csv(table)
-    print(df)
-    #transforma o DNF em 100 (significa que terminou numa posição acima dos outros)
-    numRows = df.shape[0]
-    #array = df.to_numpy
-    #print("arrei")
-    #print(array)
-    if 'DNF' in df.values:
-        df = df.replace(['DNF', "NC", "DSQ"],'100')
-    
-    print("****apos substituicao de DNF****")
-    print(df)
-    #remove linhas com dados faltando (NaN)
-    df = df.dropna()
-    print("***apos remocao de NaN")
-    print(df)
+print("variaveis independentes")
+print(X)
+print("variaveis dependentes")
+print(y)
 
-#dataset = pd.read_csv("newtables/", names = headerNames)
-#dataset.head()
+# Divide o dataset em um conjunto de treino e um de teste
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
+
+# Codificar caracteres do dataset
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+# Treina o modelo k-nn no conjunto de treino
+from sklearn.neighbors import KNeighborsClassifier
+classifier = KNeighborsClassifier(n_neighbors = 5, metric = 'minkowski', p = 2)
+classifier.fit(X_train, y_train)
+
+# Prediz o resultado dos conjuntos de teste
+y_pred = classifier.predict(X_test)
+
+# Constroi a matriz de confusão
+from sklearn.metrics import confusion_matrix, accuracy_score
+cm = confusion_matrix(y_test, y_pred)
+ac = accuracy_score(y_test, y_pred)
+
+print(ac)
